79274ad7b7f84d7c4219af821dae4bcbca7ac09c
lfontLS <lien.font@lifesurge.com>
2026-04-13 14:23:23 -0400

    added some basic logic of counter to understand within the app.vue file which dictates "home" added some global styling for the font and imported as needed

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,26 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { ref } from 'vue'
+const counter = ref(0)
+
+function incrememt() {
+  counter.value++
+}
+function decrement() {
+  counter.value--
+}
+</script>
 
 <template>
-  <h1>You did it!</h1>
-  <p>
-    Visit <a href="https://vuejs.org/" target="_blank" rel="noopener">vuejs.org</a> to read the
-    documentation
-  </p>
+  <h1>Welcome!</h1>
+  <p>This web app is created for me to understand Vue and it's environment.</p>
+  <hr />
+  <p>{{ counter }}</p>
+  <button @click="incrememt">++</button>
+  <button @click="decrement">--</button>
 </template>
 
-<style scoped></style>
+<style scoped>
+h1 {
+  color: darkred;
+}
+</style>
